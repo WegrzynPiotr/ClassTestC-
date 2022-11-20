c648266845f573c725cfb20e0b853f1828b1eb7c
Piotr Węgrzyn <piotreg8@gmail.com>
2022-11-20 17:44:55 +0100

    Add files via upload

---
+++ Colors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMĆwiczenia
+{
+    internal class Colors
+    {
+        static string currentColor = "";
+        public static string Print(string color) {
+            if(color == "blue")
+            currentColor = "Jestem Niebieski!";
+            if (color == "red")
+              currentColor =  "Jestem czerwony!";
+        return currentColor;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MMĆwiczenia
+{
+internal class Program
+{
+    static string color= "";
+    static void Main(string[] args)
+    {
+        while (color == "")
+        {
+        Console.WriteLine("Wybierz kolor!");
+        color = Console.ReadLine();
+            if (color != "blue" && color != "red")
+                color = "";
+            else
+                Console.WriteLine(Colors.Print(color));
+    }
+    }
+    }
+}
